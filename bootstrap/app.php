62fdec4abf9071fed1866faee8547ead8da9f952
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-03 13:39:18 -0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,17 +52,4 @@ $app->singleton(
 |
 */
 
-$arr = array(
-    {
-      "title": "CSE4500 Class",
-      "start": "2022-02-23T17:30:00",
-      "end": "2022-02-23T18:45:00"
-    },
-    {
-      "title": "CSE4500 Class",
-      "start": "2022-02-28T17:30:00",
-      "end": "2022-02-28T18:45:00"
-    },   
-  )
-
 return $app;
