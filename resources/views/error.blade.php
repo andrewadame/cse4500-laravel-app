450a85fcc47778fbfb648dfe0b7008423450321e
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-03 14:48:31 -0800

    Update error.blade.php

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -13,7 +13,7 @@
         <div class="container-fluid">
             <div class="error-page">
 <h2 class="headline text-warning"> 404</h2>
-<div class="error-content">
+
 <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
 <p>
  We could not find the page you were looking for.
