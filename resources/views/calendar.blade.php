46910cb8ae9966861f06f410e2339dfc6db312f1
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-03 09:32:25 -0800

    Calendar View Update I

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,19 +1,39 @@
+@section('plugins.Datatables', true)
+
 @extends('adminlte::page')
 
-@section('title', 'title')
+@section('title', 'Calendar')
 
 @section('content_header')
-    <h1>Header</h1>
+    <h1>Calendar</h1>
 @stop
 
 @section('content')
-    <p>Content here</p>
+<div class="row">
+  <div class="col-md-12">
+    <div class="card card-primary">
+      <div class="card-body p-0">
+        <div id="calendar" class="fc fc-media-screen fc-direction-ltr fc-theme-bootstrap">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.css">
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script src="//cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js" ></script>
+<script>
+$( document ).ready(function() {
+    var calendar = new FullCalendar.Calendar($('#calendar')[0], {
+      initialView: 'dayGridMonth',
+      events: '/events-feed'
+    });
+    calendar.render();
+})
+</script>
 @stop
