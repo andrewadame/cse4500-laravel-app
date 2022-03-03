4eb406d773f2ffb98c5b2bb80ec45b2b6186ab9e
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-03 14:23:10 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('board', function () {
     return view('board');
 });
 
-Route::get('/events-feed', function () {
+Route::get('events-feed', function () {
     
     $arr = array(['title' => 'CSE4500 Class', 'start' => '2022-02-23T17:30:00', 'end' => '2022-02-23T18:45:00'],
              ['title' => 'CSE4500 Class', 'start' => '2022-02-23T17:30:00', 'end' => '2022-02-23T18:45:00']);
