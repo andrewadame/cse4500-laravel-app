42c11970e1590399ed3cdbbd09cc9f5ae63db9c7
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-03 14:32:52 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -38,23 +38,5 @@ Route::get('events-feed', function () {
 });
 
 Route::fallback(function () {
-    @extends('adminlte::page')
-
-@section('title', 'title')
-
-@section('content_header')
-    <h1>Header</h1>
-@stop
-
-@section('content')
-    <p>Content here</p>
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+    
 });
