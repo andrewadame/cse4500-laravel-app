ed2cc1b1524fa110aee3aa4d82dd85b165a84f14
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-03 09:29:40 -0800

    Todos View Update I

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -1,19 +1,44 @@
 @extends('adminlte::page')
 
-@section('title', 'title')
+@section('title', 'To Do List')
 
 @section('content_header')
-    <h1>Header</h1>
+    <h1>To Do's</h1>
 @stop
 
+@section('plugins.Datatables', true)
 @section('content')
-    <p>Content here</p>
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Label</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1.</td>
+          <td>Update software</td>
+          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: 55%"></div></div></td>
+          <td><span class="badge bg-danger">55%</span></td>
+        </tr>
+        <tr>
+          <td>2.</td>
+          <td>Clean database</td>
+          <td><div class="progress progress-xs"><div class="progress-bar bg-warning" style="width: 70%"></div></div></td>
+          <td><span class="badge bg-warning">70%</span></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
 @stop
