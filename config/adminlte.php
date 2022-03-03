824a31eb7c68815d3f88b8287646e754c7965694
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-03 11:04:05 -0800

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -493,13 +493,7 @@ return [
         'text' => 'Board',
         'url'  => 'board',
         'icon' => 'fas fa-fw fa-file-alt',
-    ],
-        
-    [
-            "title": "CSE4500 Class",
-            "start": "2022-02-23T17:30:00",
-            "end": "2022-02-23T18:45:00"
-    ],
+    ],      
         
 ]
     
