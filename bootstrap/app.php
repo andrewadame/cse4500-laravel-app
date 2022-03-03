2afb5286904eeef5647b0daa1282bdcdd29a353d
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-03 13:51:06 -0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,10 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$arr = array('title' => 'CSE4500 Class', 'start' => '2022-02-23T17:30:00', 'end' => '2022-02-23T18:45:00')
-    
-echo json_encode($arr)."\n";
-
 
 /*
 |--------------------------------------------------------------------------
