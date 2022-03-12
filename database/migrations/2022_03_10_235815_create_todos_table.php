218bc5a1ff8c1086110dd20dedafd9637e725197
AndrewAdame <andrewadame526@gmail.com>
2022-03-11 20:39:00 -0800

    Update 2022_03_10_235815_create_todos_table.php

--- database/migrations/2022_03_10_235815_create_todos_table.php
+++ database/migrations/2022_03_10_235815_create_todos_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->tinyInteger('progress');
             $table->timestamps();
         });
     }
