175749710d3ec9cfc6dca30d2932c37af173eaa1
AndrewAdame <67297166+andrewadame@users.noreply.github.com>
2022-03-21 18:21:20 -0700

    Added Forced HTTPs

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,6 +36,9 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+        
+        //Force HTTPs
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
     }
 
     /**
